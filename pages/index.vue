4fdb29eb709816e2adbeab43466c3b3dbda57465
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-08-26 08:59:11 +0530

    chore: init 6 ✨
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="map" style="height: 100vh; width: 100vw" />
+</template>
+
+<script setup lang="ts">
+import maplibregl from "maplibre-gl";
+onMounted(() => {
+  new maplibregl.Map({
+    container: "map",
+    style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+    center: [0, 0],
+    zoom: 1,
+  });
+});
+// const mapRef = ref(null);
+
+// watchEffect(() => {
+//   if (mapRef.value) {
+//     new maplibregl.Map({
+//       container: mapRef.value!,
+//       style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+//       center: [0, 0],
+//       zoom: 1,
+//     });
+//   } else {
+//     // not mounted yet, or the element was unmounted (e.g. by v-if)
+//   }
+// });
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background: rgba(0, 0, 0, 0.33);
+}
+</style>
